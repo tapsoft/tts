7d7d594b6b513024dd9b7fee23e3c13f5dcdc282
Jinwoo Kim <bugkjw@kaist.ac.kr>
2019-11-16 01:51:19 +0900

    Update wav2pcm.py

--- wav2pcm.py
+++ wav2pcm.py
@@ -74,7 +74,7 @@ def main():
         wavedir = W_PATH + file_path[-39:-22]
         try:
             os.mkdir(wavedir)
-        except:
+        except Exception as ex:
             pass
 
         wavefile = W_PATH + file_path[-39:-3] + "wav"
