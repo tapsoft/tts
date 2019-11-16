ff39497675812ca05144bdba45be0236449186e2
Jinwoo Kim <bugkjw@kaist.ac.kr>
2019-11-17 00:51:51 +0900

    Update main.py

--- speaker2vec/main.py
+++ speaker2vec/main.py
@@ -53,7 +53,7 @@ def train(model, total_batch_size, queue, criterion, optimizer, device, train_be
         inputs, targets = queue.get()
         batch_size = inputs.shape[0]
 
-        logger.info('got a tensor from loader queue, shape (%d, %d, %d)'
+        logger.debug('got a tensor from loader queue, shape (%d, %d, %d)'
                     % (inputs.shape[0], inputs.shape[1], inputs.shape[2]))
 
         # no data from queue
